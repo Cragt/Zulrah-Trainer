c6b40ec4a928890ea38f239e8e4717aaf7798888
unknown <cragt369@gmail.com>
2024-06-29 00:45:43 -0500

    Move build files to root, delete dev files

--- src/app.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom'
-import App from './components/App/App'
-
-export const initialize = async () => {
-	ReactDOM.render(<App />, document.getElementById('root'))
-}
-
-initialize()
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-// Immer must be initialized with Map and Set support before the app is imported.
-
-import { enableMapSet } from 'immer'
-
-enableMapSet()
-
-import('./app').then(({ initialize }) => initialize())
